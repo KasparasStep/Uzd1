450014c9d273e0fc4d96aa72da02d98f7582685f
KasparasStep <kasparasstep@gmail.com>
2026-02-20 02:17:35 +0200

    Uzd1.cpp pakeistas i pagrindini cpp faila, kuris skirtas tik pasirinkti, kuriuo budu bus saugoma info (vector konteineryje ar c masyve)

--- Uzd1.cpp
+++ Uzd1.cpp
@@ -1,113 +1,25 @@
-#include <iomanip>
-#include <vector>
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-using std::cin;
-using std::cout;
-using std::string;
-using std::vector;
-using std::left;
-using std::right;
-using std::setw;
-using std::endl;
-using std::nth_element;
-
-struct Studentas {
-	std::string vardas = "A", pavarde = "BB";
-	//int *paz jei dinaminis masyvas
-	std::vector <int> paz;
-	int egz;
-	double rez;
-};
-void inputas(vector<Studentas>& grupe, int P);
-void outputas(vector<Studentas>& grupe);
-int pasirinkimas();
-double median(vector<int> paz);
+#include "struktura.h"
 
 int main() {
-	int P = pasirinkimas();
-	vector<Studentas>grupe;
-	inputas(grupe, P);
-	outputas(grupe);
+    int pasirinkimas;
+    std::cout << "Pasirinkite duomenu saugojimo buda:\n";
+    std::cout << "1 - Naudoti std::vector\n";
+    std::cout << "2 - Naudoti C masyvus (dinamine atmintis)\n";
+    std::cout << "Pasirinkimas: ";
+    std::cin >> pasirinkimas;
 
-}
-void inputas(vector<Studentas>& grupe, int P) {
-	for (int ii = 0; ii < 2; ii++) {
-		Studentas A;
-		cout << "Iveskite varda ir pavarde: ";
-		cin >> A.vardas >> A.pavarde;
-		cout << "Iveskite semestro ivertinimus. Kiek ju bus? ";
-		int n, sum = 0;
-		cin >> n;
-		for (int i = 0; i < n; i++) {
-			int temp;
-			cout << "Iveskite " << i + 1 << " pazymi is " << n << ":";
-			cin >> temp;
-			A.paz.push_back(temp); sum += temp;
-		}
-		cout << "Iveskite egzamino iverti: "; cin >> A.egz;
-		if (P == 1) {
-		A.rez = sum * 1.0 / (n * 1.0) * 0.4 + A.egz * 0.6;
-		}
-		else {
-		A.rez = median(A.paz) * 0.4 + A.egz * 0.6;
-		}
-		
+    if (pasirinkimas == 1) {
+        vykdytiVectorVersija();
+    }
+    else if (pasirinkimas == 2) {
+        vykdytiArrayVersija();
+    }
+    else {
+        std::cout << "Neteisingas pasirinkimas. Bandyti dar karta.\n";
+        cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cin >> pasirinkimas;
+    }
 
-		
-		grupe.push_back(A);
-		A.paz.clear();
-		
-		
-	}// galima priskirti grupej, kai turime A.rez; pushbackinam studento pavadinima
-}
-void outputas(vector<Studentas>& grupe) {
-	cout << left << setw(15) << "Vardas"
-		 << left << setw(20) << "Pavarde"
-		<< right << setw(15) << "Galutinis (Vid.)/"
-		<< right << setw(20) << "Galutinis (Med.)" << endl;
-	cout <<string(70, '-') << endl;
-
-	for (auto A : grupe) {
-		cout << left << setw(15) << A.vardas << left << setw(20) << A.pavarde
-			<< right << setw(15)
-			<< right << setw(20) << std::fixed<<std::setprecision(2)<< A.rez << endl;
-		//for (auto k : A.paz) cout << setw(3) << k;
-		//cout << setw(5) << A.egz;
-	
-	}
-}
-
-double median(vector<int> paz)
-{
-	size_t n = paz.size();
-	if (n == 0) return 0; //jei nera pazymiu
-
-	size_t mid = n / 2;  //surandamas vidurinio skaiciaus indeksas
-
-	nth_element(paz.begin(), paz.begin() + mid, paz.end());
-	double m1 = paz[mid]; //pasirenkama vidurinio skaiciaus reiksme
-
-	if (n % 2 == 1) return m1; // jei pazymiu kiekis nelyginis, mediana ir bus tas vidurinis skaicius
-
-	nth_element(paz.begin(), paz.begin() + mid - 1, paz.end()); // randamas skaicius, vienu indeksu mazesnis nei vidurys
-	double m2 = paz[mid - 1]; //priskiriama reiksme
-
-	return (m1 + m2) / 2.0; // mediana, kai pazymiu kiekis lyginis
-}
-
-int pasirinkimas() {
-		int P;
-		cout << "Pasirinkiti galutinio balo skaiciavimo metoda\n";
-		cout << "1 - Vidurkis\n";
-		cout << "2 - Mediana\n";
-		cout << "Iveskite pasirinkima: ";
-		cin >> P;
-		while (P != 1 && P != 2) {
-			cout << "Klaida: neteisingas pasirikinimas. Bandykite dar karta\n";
-			cin >> P;
-		}
-		return P;
+    return 0;
 }
