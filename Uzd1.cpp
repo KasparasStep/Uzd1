dd989550e15f5c9725b8fab23a971c1f35273ef6
KasparasStep <kasparasstep@gmail.com>
2026-02-27 23:27:25 +0200

    supaprastintas uzd1.cpp kodas

--- Uzd1.cpp
+++ Uzd1.cpp
@@ -1,30 +1,7 @@
 #include "struktura.h"
 
 int main() {
-    int pasirinkimas;
-    while (true) {
-        cout << "\n===========================================\n";
-        cout << "Pasirinkite duomenu saugojimo buda: \n";
-        cout << "1 - Naudoti std::vector\n";
-		cout << "0 - Baigti programa";
-        cout << "\n===========================================\n";
-        cout << "Pasirinkimas: ";
-
-        if (cin >> pasirinkimas) {
-            if (pasirinkimas == 0) {
-                cout << "Programa baigta.\n";
-                break;
-            }
-            if (pasirinkimas == 1) vykdytiVector();
-            
-            else cout << "Neteisingas pasirinkimas.\n";
-        }
-        else {
-		    cout << "Klaida: iveskite tinkama skaiciu.\n";
-            cin.clear();
-            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
-        }
+    vykdytiVector();
     return 0;
         
 }
