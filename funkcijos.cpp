537171a0e3e0943aba2712894e50946e6f56b6b5
KasparasStep <kasparasstep@gmail.com>
2026-03-13 22:44:58 +0200

    prideta 2 tyrimo funkcija

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -117,3 +117,28 @@ void test1() {
             << duration<double>(endGen - startGen).count() << " s\n";
     }
 }
+
+void test2(const string& failas, int metodas) {
+    vector<Studentas> visi, kieti, tinginiai;
+
+    //nuskaitymas
+	auto t1 = high_resolution_clock::now();
+	skaitytiIsFailo(failas, visi, metodas);
+	auto t2 = high_resolution_clock::now();
+
+	//skirstymas
+	splitStudentai(visi, kieti, tinginiai, metodas);
+	auto t3 = high_resolution_clock::now();
+
+	//spausdinimas i 2 failus
+	spausdintiRezultatus(kieti, metodas, "kieti_" + failas);
+	spausdintiRezultatus(tinginiai, metodas, "tinginiai_" + failas);
+	auto t4 = high_resolution_clock::now();
+
+    //rezultatai
+    cout << "\n=== " << failas << " ===\n";
+    cout << "Nuskaitymas:  " << duration<double>(t2 - t1).count() << " s\n";
+    cout << "Skirstymas:   " << duration<double>(t3 - t2).count() << " s\n";
+    cout << "Išvedimas:    " << duration<double>(t4 - t3).count() << " s\n";
+    cout << "IŠ VISO:      " << duration<double>(t4 - t1).count() << " s\n";
+}
