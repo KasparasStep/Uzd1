15f5ab2fb2bb75559b25abb622f8a98a177f1311
KasparasStep <kasparasstep@gmail.com>
2026-03-13 23:52:26 +0200

    patvarkyti pavadinimai

--- testavimas.cpp
+++ testavimas.cpp
@@ -2,11 +2,11 @@
 
 void test1() {
     vector<pair<string, int>> failai = {
-        {"studentai1000.txt", 1000},
-        {"studentai10000.txt", 10000},
-        {"studentai100000.txt", 100000},
-        {"studentai500000.txt", 1000000},
-        {"studentai1000000.txt", 10000000}
+        {"studentai1k.txt", 1000},
+        {"studentai10k.txt", 10000},
+        {"studentai100k.txt", 100000},
+        {"studentai1M.txt", 1000000},
+        {"studentai10M.txt", 10000000}
     };
 
     for (auto& [vardas, kiek] : failai) {
@@ -20,26 +20,62 @@ void test1() {
 }
 
 void test2(const string& failas, int metodas) {
-    vector<Studentas> visi, kieti, tinginiai;
+    cout << "\n=== TYRIMAS 2: Duomenų apdorojimas ===\n";
 
-    //nuskaitymas
-    auto t1 = high_resolution_clock::now();
-    skaitytiIsFailo(failas, visi, metodas);
-    auto t2 = high_resolution_clock::now();
+    // Tikrina ar failai egzistuoja prieš pradedant
+    vector<string> failai = {
+        "studentai1k.txt",
+        "studentai10k.txt",
+        "studentai100k.txt",
+        "studentai1M.txt",
+        "studentai10M.txt"
+    };
 
-    //skirstymas
-    splitStudents(visi, kieti, tinginiai, metodas);
-    auto t3 = high_resolution_clock::now();
+    for (auto& failas : failai) {
+        // Tikriname ar failas egzistuoja
+        ifstream patikrinimas(failas);
+        if (!patikrinimas) {
+            cout << "\n[!] Failas '" << failas << "' nerastas. "
+                << "Pirma paleiskite Tyrimą 1.\n";
+            continue;
+        }
+        patikrinimas.close();
 
-    //spausdinimas i 2 failus
-    spausdintiRezultatus(kieti, metodas, "kieti_" + failas);
-    spausdintiRezultatus(tinginiai, metodas, "tinginiai_" + failas);
-    auto t4 = high_resolution_clock::now();
+        cout << "\n--- " << failas << " ---\n";
+        vector<Studentas> visi, kieti, tinginiai;
 
-    //rezultatai
-    cout << "\n=== " << failas << " ===\n";
-    cout << "Nuskaitymas:  " << duration<double>(t2 - t1).count() << " s\n";
-    cout << "Skirstymas:   " << duration<double>(t3 - t2).count() << " s\n";
-    cout << "Išvedimas:    " << duration<double>(t4 - t3).count() << " s\n";
-    cout << "IŠ VISO:      " << duration<double>(t4 - t1).count() << " s\n";
+        // Bendra pradžia
+        auto visoPradzia = high_resolution_clock::now();
+
+        // 1. Nuskaitymas
+        auto t1 = high_resolution_clock::now();
+        skaitytiIsFailo(failas, visi, metodas);
+        auto t2 = high_resolution_clock::now();
+
+        // 2. Skirstymas į dvi grupes
+        splitStudents(visi, kieti, tinginiai, metodas);
+        auto t3 = high_resolution_clock::now();
+
+        // 3. Išvedimas į du failus
+        string bazinis = failas.substr(0, failas.find('.'));
+        spausdintiRezultatus(kieti, metodas, "kieti_" + bazinis + ".txt");
+        spausdintiRezultatus(tinginiai, metodas, "tinginiai_" + bazinis + ".txt");
+        auto t4 = high_resolution_clock::now();
+
+        // Rezultatai pagal užduoties reikalavimus
+        cout << "  Nuskaitymas        : "
+            << fixed << setprecision(4)
+            << duration<double>(t2 - t1).count() << " s\n";
+        cout << "  Skirstymas         : "
+            << fixed << setprecision(4)
+            << duration<double>(t3 - t2).count() << " s\n";
+        cout << "  Išvedimas į failus : "
+            << fixed << setprecision(4)
+            << duration<double>(t4 - t3).count() << " s\n";
+        cout << "  IŠ VISO            : "
+            << fixed << setprecision(4)
+            << duration<double>(t4 - visoPradzia).count() << " s\n";
+        cout << "  (Kieti: " << kieti.size()
+            << ", Tinginiai: " << tinginiai.size() << ")\n";
+    }
 }
