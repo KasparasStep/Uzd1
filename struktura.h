20343135a371b9dcddebec182ea7696a59c29d08
KasparasStep <kasparasstep@gmail.com>
2026-03-27 20:10:09 +0200

    grazinami pakeitimai, kad atlikti pirmus uzduoties punktus

--- struktura.h
+++ struktura.h
@@ -10,12 +10,8 @@
 #include <limits>
 #include <fstream>
 #include <sstream>
-#include <list>
-#include <deque>
-#include <iterator>
 
 
-//using outline
 using std::cin;
 using std::cout;
 using std::string;
@@ -61,32 +57,6 @@ struct Studentas {
 };
 
 // Funkcijų prototipai
-template <typename Container>
-void skaitytiIsFailo_T(const string& failas, Container& grupe, int metodas) {
-    ifstream in(failas);
-    if (!in) { cout << "Failas nerastas: " << failas << "\n"; return; }
-
-    string line;
-    getline(in, line); // Praleidžiame antraštę
-
-    while (getline(in, line)) {
-        if (line.empty()) continue;
-        stringstream ss(line);
-        Studentas st;
-        if (!(ss >> st.vardas >> st.pavarde)) continue;
-
-        int p;
-        while (ss >> p) st.paz.push_back(p);
-
-        if (!st.paz.empty()) {
-            st.egz = st.paz.back();
-            st.paz.pop_back();
-            apskaiciuotiPagalMetoda(st, metodas);
-            grupe.push_back(std::move(st)); // push_back tinka visiems trims tipams
-        }
-    }
-}
-
 int gautiSkaiciu(string info, int min, int max);
 string genVarda();
 string genPavarde(string vardas);
@@ -96,42 +66,13 @@ double skaiciuotiVidurki(const vector<int>& paz);
 double skaiciuotiMediana(vector<int> paz);
 void apskaiciuotiPagalMetoda(Studentas& st, int metodas);
 
-//void skaitytiIsFailo(const string& failas, vector<Studentas>& grupe, int metodas);
+void skaitytiIsFailo(const string& failas, vector<Studentas>& grupe, int metodas);
 void spausdintiRezultatus(const vector<Studentas>& grupe, int rodyti, const string& failas);
 void vykdytiVector();
 
 void genFaila(const string& failas, int kiek);
-//void splitStudents(const vector<Studentas>& visi, vector<Studentas>& kieti,
-//	vector <Studentas>& tiniginiai, int metodas);
+void splitStudents(const vector<Studentas>& visi, vector<Studentas>& kieti,
+	vector <Studentas>& tiniginiai, int metodas);
 
 void test1();
 void test2(const string& failas, int metodas);
-
-
-// STRATEGIJA 1
-
-template <typename Container>
-void split_Strategy1(const Container& visi, Container& kieti, Container& tinginiai, int metodas) {
-    auto galutinis = [&](const Studentas& st) { return (metodas == 2) ? st.gal_med : st.gal_vid; };
-
-    std::copy_if(visi.begin(), visi.end(), std::back_inserter(kieti), [&](const Studentas& st) { return galutinis(st) >= 5.0; });
-    std::copy_if(visi.begin(), visi.end(), std::back_inserter(tinginiai), [&](const Studentas& st) { return galutinis(st) < 5.0; });
-}
-
-// STRATEGIJA 3 (std::vector ir std::deque)
-// Naudoja partition ir erase
-
-template <typename Container>
-void split_Strategy3_VD(Container& visi, Container& tinginiai, int metodas) {
-    auto galutinis = [&](const Studentas& st) { return (metodas == 2) ? st.gal_med : st.gal_vid; };
-
-    // stable_partition sustumia visus kietus į priekį, o tinginius į galą.
-    // 'it' yra riba, nuo kurios prasideda tinginiai.
-    auto it = std::stable_partition(visi.begin(), visi.end(), [&](const Studentas& st) { return galutinis(st) >= 5.0; });
-
-    // Perkeliam tinginius į naują konteinerį greituoju (move) būdu
-    tinginiai.insert(tinginiai.end(), std::make_move_iterator(it), std::make_move_iterator(visi.end()));
-
-    // Ištriname tinginius iš originalaus konteinerio (jame lieka tik kieti)
-    visi.erase(it, visi.end());
-}
